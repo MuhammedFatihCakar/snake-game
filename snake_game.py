505922992f170fcacd982656ca8259ba4377f6ed
Muhammed Fatih Çakar <114261655+MuhammedFatihCakar@users.noreply.github.com>
2024-06-17 00:07:12 +0300

    İlk oyun dosyası yüklendi

---
+++ snake_game.py
@@ -0,0 +1,135 @@
+import pygame
+import time
+import random
+
+# Pygame'i başlat
+pygame.init()
+
+# Renkleri tanımla
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+# Ekran boyutlarını belirle
+dis_width = 800
+dis_height = 600
+
+# Ekranı oluştur
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Eysan Oyunu')
+
+# Yılan simgesini yükle ve ayarla
+icon = pygame.image.load('eysan.jpg.jpg')
+pygame.display.set_icon(icon)
+
+# Saat ve hız ayarları
+clock = pygame.time.Clock()
+snake_block = 10
+snake_speed = 15
+
+# Yazı tiplerini ayarla
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+
+# Puanı gösterme fonksiyonu
+def Your_score(score):
+    value = score_font.render("Skor: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+
+# Yılanı çizme fonksiyonu
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+# Mesaj gösterme fonksiyonu
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
+# Ana oyun döngüsü
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(blue)
+            message("Oyunu kaybettin! Q-Quit veya C-Continue", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)  # Arka plan rengini siyah yap
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])  # Yemeği yeşil yap
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+# Oyunu başlat
+gameLoop()
+
